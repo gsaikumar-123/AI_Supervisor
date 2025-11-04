4eb5baf05884cdb006fd7e8c0efd6739e61bf3d5
G SAI KUMAR <gsaikumar2505@gmail.com>
2025-11-04 07:41:20 +0530

    minor changes

--- src/server.js
+++ src/server.js
@@ -15,4 +15,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'test-call.html'));
 });
 
+app.get('/health', (req, res) => res.json({ status: 'ok' }));
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err && err.stack ? err.stack : err);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+});
+
 app.listen(config.port, () => console.log(`Server running on http://localhost:${config.port}`));
